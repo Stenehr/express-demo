fd1290f2e973e9fd9a1c3dbac5020197829d8bdc
Sten Ehrberg <sten.ehrberg@gmail.com>
2021-08-30 23:26:23 +0300

    added genre handlers. independent

--- index.js
+++ index.js
@@ -9,12 +9,25 @@ const courses = [
     { id: 2, name: "course2" }
 ];
 
+const genres = [
+    { id: 1, name: "Action" },
+    { id: 2, name: "Horror" }
+]
+
 function validateCourse(course) {
     const schema = Joi.object({
         name: Joi.string().required().min(3)
     });
 
     return schema.validate(course);
+}
+
+function validateGenre(genre) {
+    const schema = Joi.object({
+        name: Joi.string().required()
+    });
+
+    return schema.validate(genre);
 }
 
 app.get('/', (req, res) => {
@@ -66,6 +79,45 @@ app.delete('/api/courses/:id', (req, res) => {
 
     res.send(course);
 })
+
+app.get('/api/genres', (req, res) => {
+    res.send(genres);
+});
+
+app.post('/api/genres', (req, res) => {
+    const { error } = validateGenre(req.body);
+    if (error) return res.status(404).send(error.details[0].message);
+
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name
+    }
+
+    genres.push(genre);
+
+    res.status(201).send(genre);
+});
+
+app.put('/api/genres/:id', (req, res) => {
+    const genre = genres.find((g) => g.id === Number(req.params.id));
+    if (!genre) return res.status(404).send("Genre not found");
+
+    const { error } = validateGenre(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    genre.name = req.body.name;
+
+    res.send(genre);
+});
+
+app.delete('/api/genres/:id', (req, res) => {
+    const genre = genres.find((g) => g.id === Number(req.params.id));
+    if (!genre) return res.status(404).send("Genre not found");
+
+    genres.splice(genres.indexOf(genre), 1);
+
+    res.send(genre);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening ${port}...`));
