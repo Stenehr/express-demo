4d815929bca92064c9a7e72f3f2cfdba11c8fd00
Sten Ehrberg <sten.ehrberg@gmail.com>
2021-09-04 15:10:54 +0300

    trying mongoose, basic filtering and sorting

--- demos/mongoose.js
+++ demos/mongoose.js
@@ -30,7 +30,14 @@ async function createCourse() {
 // createCourse();
 
 async function getCourses() {
-    const courses = await Course.find();
+    const courses = await Course
+        .find({
+            author: 'Tim',
+            isPublished: true
+        })
+        .limit(10)
+        .sort({ name: 1 })
+        .select( { name: 1, tags: 1 });
     console.log(courses);
 }
 
