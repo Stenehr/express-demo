7142885a259a7a03c7c88229e2717045636669f1
Sten Ehrberg <sten.ehrberg@gmail.com>
2021-09-04 15:03:24 +0300

    trying mongoose, saving documents

--- demos/mongoose.js
+++ demos/mongoose.js
@@ -1,10 +1,30 @@
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb:localhost/playground')
+mongoose.connect('mongodb://localhost:27017/playground')
     .then(() => console.log('connected...'))
     .catch(err => console.log(err));
     
 const courseSchema = new mongoose.Schema({
-    name: String
-})
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+async function createCourse() {
+    const course = new Course({
+        name: 'Test Course',
+        author: 'Tim',
+        tags: ['test', 'backend'],
+        isPublished: true
+    });
+
+    const result = await course.save();
+    console.log(result);
+}
+
+createCourse();
