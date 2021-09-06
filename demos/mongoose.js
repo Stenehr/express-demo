e7fc08d62156b7407c78f7b9fce78532bc719648
Sten Ehrberg <sten.ehrberg@gmail.com>
2021-09-06 23:05:11 +0300

    trying mongoose, validations

--- demos/mongoose.js
+++ demos/mongoose.js
@@ -1,35 +1,72 @@
+const mongoose = require("mongoose");
 
-const mongoose = require('mongoose');
+mongoose
+    .connect("mongodb://localhost:27017/playground")
+    .then(() => console.log("connected..."))
+    .catch((err) => console.log(err));
 
-mongoose.connect('mongodb://localhost:27017/playground')
-    .then(() => console.log('connected...'))
-    .catch(err => console.log(err));
-    
 const courseSchema = new mongoose.Schema({
-    name: String,
+    name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 50,
+        // match: /pattern/
+    },
+    category: {
+        type: String,
+        required: true,
+        enum: ['web', 'mobile'],
+        lowercase: true,
+        trim: true
+    },
     author: String,
-    tags: [ String ],
+    tags: {
+        type: Array,
+        validate: {
+            validator: function(v) {
+                return !!v && v.length > 0;
+            },
+            message: 'Atleast one tag required'
+        }
+    },
     date: { type: Date, default: Date.now },
     isPublished: Boolean,
-    price: Number
+    price: {
+        type: Number,
+        required: function () {
+            return this.isPublished;
+        },
+        min: 10,
+        max: 100,
+        get: v => Math.round(v),
+        set: v => Math.round(v)
+    }
 });
 
-const Course = mongoose.model('Course', courseSchema);
+const Course = mongoose.model("Course", courseSchema);
 
 async function createCourse() {
     const course = new Course({
-        name: 'Test Course',
-        author: 'Bill',
-        tags: ['test', 'backend'],
-        price: 25,
-        isPublished: false
+        name: "Test Course",
+        author: "Bill",
+        tags: ["test"],
+        price: 25.04213455,
+        isPublished: true,
+        category: 'WEb',
     });
 
-    const result = await course.save();
-    console.log(result);
+    try {
+        const result = await course.save();
+        console.log(result);
+    } catch (ex) {
+        for (field in ex.errors) {
+            console.log(ex.errors[field].message);
+        }
+    }
 }
 
-// createCourse();
+createCourse();
 
 async function getCourses() {
     const courses = await Course
@@ -57,46 +94,46 @@ async function getCourses() {
         .limit(10)
         // .skip(1)
         .sort({ name: 1 })
-        .select( { name: 1, author: 1, price: 1, isPublished: 1 })
-        // .count();
+        .select({ name: 1, author: 1, price: 1, isPublished: 1 });
+    // .count();
     console.log(courses);
 }
 
 // getCourses();
 
-async function updateCourse(id) {
-    const course = await Course.findById(id);
-    course.set({
-        author: 'Someone else',
-        tags: [ ...course.tags, 'Added tag' ]
-    });
+// async function updateCourse(id) {
+//     const course = await Course.findById(id);
+//     course.set({
+//         author: 'Someone else',
+//         tags: [ ...course.tags, 'Added tag' ]
+//     });
 
-    const result = await course.save();
-    console.log(result);
-}
+//     const result = await course.save();
+//     console.log(result);
+// }
 
-async function updateFirst(id) {
-    // const result = await Course.updateOne({ _id: id }, {
-    //     $set: {
-    //         author: 'Bill changed'
-    //     }
-    // });
-    // console.log(result);
+// async function updateFirst(id) {
+//     // const result = await Course.updateOne({ _id: id }, {
+//     //     $set: {
+//     //         author: 'Bill changed'
+//     //     }
+//     // });
+//     // console.log(result);
 
-    const course = await Course.findByIdAndUpdate(id, {
-        $set: {
-            author: 'Bill changed second'
-        }
-    }, { new: true });
-    console.log(course);
-}
+//     const course = await Course.findByIdAndUpdate(id, {
+//         $set: {
+//             author: 'Bill changed second'
+//         }
+//     }, { new: true });
+//     console.log(course);
+// }
 
-// updateFirst('6134bf045a5791af513f0173');
+// // updateFirst('6134bf045a5791af513f0173');
 
-async function removeCourse(id) {
-    const result = await Course.deleteOne({ _id: id });
-    // const course = await Course.findByIdAndRemove(id);
-    console.log(result);
-}
+// async function removeCourse(id) {
+//     const result = await Course.deleteOne({ _id: id });
+//     // const course = await Course.findByIdAndRemove(id);
+//     console.log(result);
+// }
 
-removeCourse('6134bf045a5791af513f0173');
+// removeCourse('6134bf045a5791af513f0173');
